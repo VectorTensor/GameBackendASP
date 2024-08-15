fa480318979b66652224d490dcb5ecf4e54edeb6
VectorTensor <comppro54@gmail.com>
2024-08-15 22:16:04 +0545

    feat(register): basic register page component done

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Form,ReactiveFormsModule ,FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit{
+
+  registerForm: FormGroup = this.fb.group({
+    email:'',
+    password: '',
+    repeatPassword: ''
+  })
+
+  constructor(private fb: FormBuilder){
+
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
+  onSubmit() {
+
+    if(this.registerForm.valid){
+
+      console.log('Registration successful', this.registerForm.value);
+
+
+    }
+    else{
+
+      console.error('Form is invalid');
+
+    }
+
+
+
+  }
+
+}
