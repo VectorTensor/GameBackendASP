664fc8e71b48d877f3061841f680ec9c59adc7e9
VectorTensor <comppro54@gmail.com>
2024-08-15 21:15:25 +0545

    chore

--- GameBackend/obj/Debug/net8.0/GameBackend.AssemblyInfo.cs
+++ GameBackend/obj/Debug/net8.0/GameBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d92bc6776f6d5abdeaff169f967c77eb7bd25bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686eedf2acb18bea733a9faea12bb2c48bad08d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
