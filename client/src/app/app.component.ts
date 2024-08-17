f353cd654aa27d62ead461a7c5f5742fe27c413f
VectorTensor <prayashthapa15@gmail.com>
2024-08-17 12:04:34 +0545

    feat(nav): nav bar added along with proper routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {HttpClientModule} from "@angular/common/http";
 import {ScoresComponent} from "./scores/scores.component";
 import { RegisterComponent } from './register/register.component';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatIcon} from "@angular/material/icon";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatAnchor, MatButton} from "@angular/material/button";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, ScoresComponent, RegisterComponent],
+  imports: [RouterOutlet, HttpClientModule, ScoresComponent, RegisterComponent, MatToolbar, MatMenuTrigger, MatIcon, MatMenu, MatTabsModule, MatAnchor, RouterLink, MatButton, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'client';
+
+
+
 }
